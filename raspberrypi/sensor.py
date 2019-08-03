4d4457c7bc0c0789fc1e077dd3c0ae2092677f7b
tingwood <tingwood@hotmail.com>
2019-08-03 15:12:45 +0800

    update

--- raspberrypi/sensor.py
+++ raspberrypi/sensor.py
@@ -83,3 +83,182 @@ class Relay(Sensor):
 
     def untrigger(self):
         GPIO.output(self.pins[0], GPIO.LOW)
+
+
+class InfraredTracker(Sensor):
+    '''
+    Infrared tracker sensor
+    if the reflection is strong enough, output GPIO.HIGH
+    otherwise output GPIO.LOW (e.g. detect black line)
+    '''
+
+    def __init__(self, pin):
+        pins = [pin]
+        super(InfraredTracker, self).__init__(pins)
+        GPIO.setup(self.pins, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+    def getresult(self):
+        return GPIO.input(self.pins[0])
+
+
+class DHT11(Sensor):
+    '''
+    https://blog.csdn.net/xindoo/article/details/53544699
+    '''
+
+    def __init__(self, pin):
+        pins = [pin]
+        super(DHT11, self).__init__(pins)
+
+    # return humidity and temperature
+    def getresult(self):
+        pin = self.pins[0]
+        data = []
+        GPIO.setup(pin, GPIO.OUT)
+        GPIO.output(pin, GPIO.HIGH)
+        GPIO.output(pin, GPIO.LOW)
+        time.sleep(0.04)
+        GPIO.output(pin, GPIO.HIGH)
+
+        GPIO.setup(pin, GPIO.IN)
+
+        while GPIO.input(pin) == GPIO.LOW:
+            continue
+        while GPIO.input(pin) == GPIO.HIGH:
+            continue
+        i = 0
+        while i < 40:
+            cnt = 0
+            while GPIO.input(pin) == GPIO.LOW:
+                continue
+            while GPIO.input(pin) == GPIO.HIGH:
+                cnt += 1
+                if cnt > 100:
+                    break
+            if cnt < 12:
+                data.append(0)
+            else:
+                data.append(1)
+            i += 1
+
+        humidity_bit = data[0:8]
+        humidity_point_bit = data[8:16]
+        temperature_bit = data[16:24]
+        temperature_point_bit = data[24:32]
+        check_bit = data[32:40]
+
+        humidity = 0
+        humidity_point = 0
+        temperature = 0
+        temperature_point = 0
+        check = 0
+
+        for i in range(8):
+            humidity += humidity_bit[i] * 2 ** (7 - i)
+            humidity_point += humidity_point_bit[i] * 2 ** (7 - i)
+            temperature += temperature_bit[i] * 2 ** (7 - i)
+            temperature_point += temperature_point_bit[i] * 2 ** (7 - i)
+            check += check_bit[i] * 2 ** (7 - i)
+
+        tmp = humidity + humidity_point + temperature + temperature_point
+
+        if check == tmp:
+            print("temperature :", temperature, "*C, humidity:", humidity, "%")
+            return humidity, temperature
+        else:
+            return 0, 0
+
+
+class Ultrasonic(Sensor):
+    '''
+    https://blog.csdn.net/weixin_41860080/article/details/86766856
+    HY-SRF05 HY-SR04
+    '''
+
+    def __init__(self, trigpin, echopin):
+        pins = [trigpin, echopin]
+        super(Ultrasonic, self).__init__(pins)
+        GPIO.setup(self.pins[0], GPIO.OUT)
+        GPIO.setup(self.pins[1], GPIO.IN)
+        time.sleep(0.02)
+        GPIO.output(self.pins[0], GPIO.LOW)
+
+    def distance(self):
+        GPIO.output(self.pins[0], GPIO.HIGH)
+        time.sleep(0.00001)
+        GPIO.output(self.pins[0], GPIO.LOW)
+        while GPIO.input(self.pins[1]) == GPIO.LOW:
+            start_time = time.time()
+        while GPIO.input(self.pins[1]) == GPIO.HIGH:
+            end_time = time.time()
+        t = end_time - start_time
+        distance = 17150 * t
+        print("Measured Distance is:", distance, "cms.")
+        return distance
+
+
+class InfraredObstacle(Sensor):
+    '''
+    if detect obstacle, the input is GPIO.LOW
+    '''
+
+    def __init__(self, pin):
+        pins = [pin]
+        super(InfraredObstacle, self).__init__(pins)
+        GPIO.setup(self.pins, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+    def obstacle(self):
+        if GPIO.input(self.pins[0]) == GPIO.LOW:
+            return True
+
+
+class TouchSwitcher(Sensor):
+    '''
+    触摸开关
+    if touch, the input is GPIO.LOW
+    '''
+    status = False
+    callback()
+
+    def __init__(self, pin, callback):
+        pins = [pin]
+        super(TouchSwitcher, self).__init__(pins)
+        GPIO.setup(self.pins, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        self.status = False
+        self.callback = callback
+
+    def status(self):
+        return self.status
+
+    def __detect_touch(self):
+        while 1:
+            touch = False
+            while GPIO.input(self.pins[0]) == GPIO.HIGH:
+                continue
+            while GPIO.input(self.pins[0]) == GPIO.LOW:
+                touch = True
+            if touch:
+                self.status = not self.status
+
+
+class RGBLed(Sensor):
+    '''
+    RGB Leb light.
+    '''
+
+    def __init__(self, rpin, gpin, bpin):
+        pins = [rpin, gpin, bpin]
+        super(RGBLed, self).__init__(pins)
+        GPIO.setup(self.pins, GPIO.OUT)
+
+    def red(self):
+        GPIO.output(self.pins, [1, 0, 0])
+
+    def green(self):
+        GPIO.output(self.pins, [0, 1, 0])
+
+    def blue(self):
+        GPIO.output(self.pins, [0, 0, 1])
+
+    def turnoff(self):
+        GPIO.output(self.pins, [0, 0, 0])
---
+++ raspberrypi/voice_ctrl.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+"""A demo of the Google CloudSpeech recognizer."""
+import argparse
+import locale
+import logging
+
+from aiy.board import Board, Led
+from aiy.cloudspeech import CloudSpeechClient
+
+
+def get_hints(language_code):
+    if language_code.startswith('en_'):
+        return ('water flower',
+                'goodbye')
+    return None
+
+def locale_language():
+    language, _ = locale.getdefaultlocale()
+    return language
+
+def main():
+    logging.basicConfig(level=logging.DEBUG)
+
+    parser = argparse.ArgumentParser(description='Assistant service example.')
+    parser.add_argument('--language', default=locale_language())
+    args = parser.parse_args()
+
+    logging.info('Initializing for language %s...', args.language)
+    hints = get_hints(args.language)
+    client = CloudSpeechClient()
+    with Board() as board:
+        while True:
+            board.button.wait_for_press()
+            board.led.state = Led.ON
+            while True:
+                if hints:
+                    logging.info('Say something, e.g. %s.' % ', '.join(hints))
+                else:
+                    logging.info('Say something.')
+                text = client.recognize(language_code=args.language,
+                                        hint_phrases=hints)
+                if text is None:
+                    logging.info('Sorry, You said nothing.')
+                    continue
+
+                logging.info('You said: "%s"' % text)
+                text = text.lower()
+                if 'water flower' in text:
+                    logging.info('watering flower')
+                elif 'goodbye' in text:
+                    exit(0)
+                board.led.state = Led.OFF
+                break
+
+if __name__ == '__main__':
+    main()
