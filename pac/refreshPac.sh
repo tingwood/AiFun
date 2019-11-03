2a48fbe6d72be58ac2e8c7e90b646e8e90e89dcc
tingwood <tingwood.hotmail.com>
2019-11-03 16:46:11 +0800

    update pac

--- pac/refreshPac.sh
+++ pac/refreshPac.sh
@@ -1,1 +1,1 @@
-genpac --proxy="http 192.168.8.101:3128" --gfwlist-proxy="http 192.168.8.101:3128" -o ./autoproxy.pac --gfwlist-url="https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt"
+genpac --proxy="http://192.168.8.101:3128" --gfwlist-proxy="http://192.168.8.101:3128" -o autoproxy.pac --gfwlist-url="https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt"
