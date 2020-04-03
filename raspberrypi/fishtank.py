f308f29051af509e190f7dd52b85db7d933219db
tingwood <tingwood@hotmail.com>
2020-04-03 21:25:56 +0800

    fishtank commit

---
+++ raspberrypi/fishtank.py
@@ -0,0 +1,171 @@
+import RPi.GPIO as GPIO
+import time
+from apscheduler.schedulers.background import BackgroundScheduler
+import logging as log
+
+
+class Fishtank:
+    '''
+    fish tank
+    '''
+    runmode = 0  #0-normal, 1-change water
+    light_status = 0  #0-off, 1-on
+    pump_status = 0  #0-off, 1-on
+    pump_ext_status = 0
+    heater_status = 0  #0-off, 1-on
+    uv_status = 0
+    temprature = 0  #temp get from sensor
+    temp_offset = 0  #temp sensor calibration
+    temp_heater_on = 16  #below this temp, heater on
+    temp_heater_off = 22  #above this temp, heater off
+
+    pins = [0, 4, 17, 18, 27]
+    scheduler = BackgroundScheduler()
+    jobs = []
+
+    def __init__(self, lightPin,pumpPin,pumbExtPin,uvPin,heaterPin):
+        self.pins = [lightPin,pumpPin,pumbExtPin,uvPin,heaterPin]
+        GPIO.setup(self.pins, GPIO.OUT)
+        self.pump_on()
+        # 采用corn的方式
+        job = self.scheduler.add_job(self.pump_ext_on_comb, 'cron', hour='10')
+        self.jobs.append(job)
+        job = self.scheduler.add_job(self.pump_ext_off_comb, 'cron', hour='11')
+        self.jobs.append(job)
+        job = self.scheduler.add_job(self.uv_on,
+                                     'cron',
+                                     day_of_week='sat',
+                                     hour='10')
+        self.jobs.append(job)
+        job = self.scheduler.add_job(self.uv_off,
+                                     'cron',
+                                     day_of_week='sat',
+                                     hour='10',
+                                     minute='30')
+        self.jobs.append(job)
+        job = self.scheduler.add_job(self.get_temprature,
+                                     'interval',
+                                     minutes=5)
+        self.jobs.append(job)
+        self.scheduler.start()
+
+    def set_runmode(self, mode):
+        if mode < 0 or mode > 1:
+            return
+        self.runmode = mode
+        if mode == 0:
+            log.info("switch to auto-running mode")
+            for job in self.jobs:
+                job.resume()
+        if mode == 1:
+            log.info("switch to changing water mode")
+            for job in self.jobs:
+                job.pause()
+            self.uv_off()
+            self.pump_ext_off()
+            self.pump_off()
+            self.light_on()
+            self.heater_on()
+
+    def get_runmode(self):
+        return self.runmode
+
+    def light_on(self):
+        pin = self.pins[0]
+        if pin > 0:
+            GPIO.output(pin, GPIO.HIGH)
+        self.light_status = 1
+        log.info("light on")
+
+    def light_off(self):
+        pin = self.pins[0]
+        if pin > 0:
+            GPIO.output(pin, GPIO.LOW)
+        self.light_status = 1
+        log.info("light off")
+
+    def pump_on(self):
+        pin = self.pins[1]
+        if pin > 0:
+            GPIO.output(pin, GPIO.HIGH)
+        self.pump_status = 1
+        log.info("pump on")
+
+    def pump_off(self):
+        pin = self.pins[1]
+        if pin > 0:
+            GPIO.output(pin, GPIO.LOW)
+        self.pump_status = 0
+        log.info("pump off")
+
+    def pump_ext_on(self):
+        pin = self.pins[2]
+        if pin > 0:
+            GPIO.output(pin, GPIO.HIGH)
+        self.pump_ext_status = 1
+        log.info("external pump on")
+
+    def pump_ext_off(self):
+        pin = self.pins[2]
+        if pin > 0:
+            GPIO.output(pin, GPIO.LOW)
+        self.pump_ext_status = 0
+        log.info("external pump off")
+
+    def uv_on(self):
+        pin = self.pins[3]
+        if pin > 0:
+            GPIO.output(pin, GPIO.HIGH)
+        self.uv_status = 1
+        log.info("UV light on")
+
+    def uv_off(self):
+        pin = self.pins[3]
+        if pin > 0:
+            GPIO.output(pin, GPIO.LOW)
+        self.uv_status = 0
+        log.info("UV light off")
+
+    def heater_on(self):
+        pin = self.pins[4]
+        if pin > 0:
+            GPIO.output(pin, GPIO.HIGH)
+        self.heater_status = 1
+        log.info("heater on")
+
+    def heater_off(self):
+        pin = self.pins[4]
+        if pin > 0:
+            GPIO.output(pin, GPIO.HIGH)
+        self.heater_status = 0
+        log.info("heater off")
+
+    def get_temprature(self):
+        temp = self.temprature + self.temp_offset
+        log.info("Current temp is %d", temp)
+        if temp < self.temp_heater_on:
+            log.info("Current temp is lower than %d", self.temp_heater_on)
+            self.heater_on()
+        if temp > self.temp_heater_off:
+            log.info("Current temp is higher than %d", self.temp_heater_off)
+            self.heater_off()
+        return temp
+
+    def get_status(self):
+        st = {}
+        st['runmode'] = self.runmode
+        st['light'] = self.light_status
+        st['heater'] = self.heater_status
+        st['uvlight'] = self.uv_status
+        st['pump'] = self.pump_status
+        st['pump_ext'] = self.pump_ext_status
+        st['temprature'] = self.get_temprature()
+        return st
+
+    def pump_ext_on_comb(self):
+        self.pump_ext_on()
+        self.pump_off()
+
+    def pump_ext_off_comb(self):
+        self.pump_ext_off()
+        self.pump_on()
--- raspberrypi/pi_service.py
+++ raspberrypi/pi_service.py
@@ -1,93 +1,66 @@
 # -*- coding: UTF-8 -*-
 import urllib.request as http
 from bs4 import BeautifulSoup
-from flask import Flask,request,jsonify
+from flask import Flask, request, jsonify
 import json
 import re
 import random
 import logging
-import pi_actions as pi
+import RPi.GPIO as GPIO
+from fishtank import Fishtank
 
-app=Flask(__name__) 
-
+app = Flask(__name__)
 logging.basicConfig(filename='./log/info.log',\
                             filemode='w',\
                             format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',\
                             datefmt='%H:%M:%S',\
                             level=logging.INFO)
 
-@app.route('/pi',methods=['GET'])
-def hello():
-    action= request.args.get('action').strip()
-    if action=="hello":
-        return "Hello"
-    elif action=="temp":
-        hum,temp=pi.get_temp();
-        return "temp",temp
-    else:
-        raise ServiceException("Unsupport actions", status_code=500)
-    
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+fishtank = Fishtank(0,4,17,18,27)
+@app.route('/fishtank', methods=['GET'])
+def get_fishtank_status():
+    ret = fishtank.get_status()
+    print(ret)
+    return jsonify(ret)
 
-@app.route('/book',methods=['GET'])      
-def bookByIsbn():
-    isbn = request.args.get('isbn').strip()
-    logging.info(request)
-    if isbn is None:
-        raise ServiceException('Missing parameters.', status_code=500)
-    else:        
-        isIsbn=isbnRegex.match(isbn)
-        if isIsbn is None:
-            raise ServiceException('Invalid isbn.', status_code=500)
-        
-        ret={}
-        try:
-            book=doubanCrawler(isbn)
-            #print book
-            ret=dict(ret.items()+book.items())
-        except Exception as e:
-            logging.info('Creep bookinfo from douban failed. Reason as below:')
-            logging.info(e)
-        '''
-        try:
-            book=goodReadsCrawler(isbn)
-            #print book
-            ret=dict(ret.items()+book.items())
-        except Exception as e:
-            logging.info('Creep bookinfo from goodreads failed. Reason as below:')
-            logging.info(e)
-        '''
-        if ret=={}:
-            raise ServiceException("Book not found", status_code=404)
-        return jsonify(ret)
+
+@app.route('/fishtank', methods=['POST'])
+def set_fishtank_runmode():
+    mode = request.args.get('mode').strip()
+    if mode == '0':
+        fishtank.set_runmode(0)
+    elif mode == '1':
+        fishtank.set_runmode(1)
+    else:
+        raise ServiceException("Unsupport mode")
+    return jsonify(fishtank.get_status())
 
 
 class ServiceException(Exception):
     status_code = 400
 
-    def __init__(self, message, status_code=None, payload=None):        
+    def __init__(self, message, status_code=None, payload=None):
         super(ServiceException, self).__init__(message)
         self.message = message
         if status_code is not None:
             self.status_code = status_code
         self.payload = payload
-        
 
     def to_dict(self):
         rv = dict(self.payload or ())
         rv['message'] = self.message
         return rv
-    
-    
+
+
 @app.errorhandler(ServiceException)
 def handleServiceExp(error):
     response = jsonify(error.to_dict())
     response.status_code = error.status_code
     return response
-            
-            
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=18082)              #启动socket
-
-
-
+    app.run(host='0.0.0.0', port=18082)  #启动socket
 
